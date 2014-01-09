a818003addc872f2daf9d1d56c275c419bb11d1d
auomoto <auomoto@gmail.com>
2014-01-09 11:52:28 -0800

    Added #defines for status byte flags

--- pca9543.h
+++ pca9543.h
@@ -1,1 +1,13 @@
-#define PCA9543_BASEADDR 0x70		// Shift left one and add r/w bit
+/*
+	The PCA9543 is an I2C bus switch. We're using it here to
+	select between two I2C items (PN532 NFC reader) that does
+	not have selectable I2C addresses.
+*/
+
+//#include <avr/io.h>
+#include <stdint.h>
+#define PCA9543_BASEADDR 0x70		// A0 and A1 grounded. Shift left one and add r/w bit.
+
+void pca9543SelectChannel(uint8_t channel);
+
+uint8_t pca9543InterruptChannel(void);
